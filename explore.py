df454787d2e400490f1d89e8e87acfdaaadd4c06
ArturoAmaya <a1amaya@ucsd.edu>
2022-05-23 13:14:08 -0700

    found how to read csv files

--- explore.py
+++ explore.py
@@ -1,3 +1,6 @@
 import csv
-msg = "Hello World"
-print(msg)  
+
+with open('MH-Degree-plan-Muir-CS26.csv', newline='') as csvfile:
+    readboi = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in readboi:
+        print(', '.join(row))
