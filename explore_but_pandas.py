d164eb4ede601c0b33bac58ac93ca71a2996465d
ArturoAmaya <a1amaya@ucsd.edu>
2022-05-23 14:39:15 -0700

    got pandas to work... first thoughts uploaded

--- explore_but_pandas.py
+++ explore_but_pandas.py
@@ -2,5 +2,9 @@ import pandas as pd
 
 df = pd.read_csv("MH-Degree-plan-Muir-CS26.csv")
 
-
 print(df)
+print(df.iloc[0])
+print(df.iloc[1])
+print(df.iloc[2])
+
+df.to_csv('MH-Degree-plan-Muir-CS26AUTOMATEDpandas.csv', index=False)
