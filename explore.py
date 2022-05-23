06f1e3f03bd7767edd44e08c75538227275a63f0
ArturoAmaya <a1amaya@ucsd.edu>
2022-05-23 14:15:24 -0700

    got pandas working on my local machine

--- explore.py
+++ explore.py
@@ -1,6 +1,14 @@
 import csv
 
+# read
 with open('MH-Degree-plan-Muir-CS26.csv', newline='') as csvfile:
-    readboi = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    readboi = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in readboi:
         print(', '.join(row))
+# write
+with open('MH-Degree-plan-Muir-CS26AUTOMATED.csv', 'w', newline='') as csvfile:
+    writeboi = csv.writer(csvfile, delimiter=',', quotechar = '"', quoting=csv.QUOTE_MINIMAL)
+    writeboi.writerow(['Curriculum'] + ['Computer Science'] + ['']*9)
+    writeboi.writerow(['Degree Plan', 'Computer Science/ Muir'] + ['']*9)
+    writeboi.writerow(['Institution', 'University of California, San Diego']+['']*9)
+    writeboi.writerow(['Course ID','Course Name','Prefix','Number','Prerequisites','Corequisites','Strict-Corequisites','Credit Hours','Institution','Canonical Name','Term'])
---
+++ explore_but_pandas.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_csv("MH-Degree-plan-Muir-CS26.csv")
+
+
+print(df)
