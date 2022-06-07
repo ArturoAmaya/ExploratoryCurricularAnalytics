973226a8a926f5bf96a793f5974da203651b651a
Sean <ussa.say@gmail.com>
2022-06-07 15:41:47 -0700

    Improve `parse_course_name` per investigations

--- parse_course_name.py
+++ parse_course_name.py
@@ -3,41 +3,39 @@ Split course names from the academic plans into its subject and number to get
 its prerequisites.
 """
 
-from typing import Tuple
+import re
+from typing import Literal, Optional, Tuple
 
 
-def parse_course_name(name: str) -> Tuple[str, str]:
+def parse_course_name(
+    name: str,
+) -> Optional[Tuple[str, str, Optional[Literal["L", "X"]]]]:
     """
     Attempts to parse course name strings such as "MATH 10A/20A" into the course
-    subject or department, which Curricular Analytics calls the prefix, and the
-    course number.
+    subject, which Curricular Analytics calls the prefix, and number.
 
-    Returns a tuple of the prefix and number. If there isn't a prefix or number,
-    use an empty string.
+    Returns a tuple of the prefix, number, and either L/X if the course is
+    actually two courses and needs to be split as a physics lab or language
+    analysis section.
 
     This function is in its own file because parsing course names from academic
-    plans might get quite complicated. For example, in the CS26 curriculum
-    example file,
-
-    - `CSE 197` has a prefix `CSE` and number `197/Elective`.
-    - `Elective/Tech E1` has a prefix `CSE` with no number.
-    - `SYSTEMS/NETW` has no prefix nor number.
-
-    There are also numerous oddities in the academic plans CSV file's "Course"
-    column (column D). Here's just a sampling.
-
-    - BI30's ERC plan has `MATH 10A/20A` and `PHYS 1A&1AL`.
-    - AN26's Muir plan has `MUIR UD Elective`.
-    - AN26's Revelle plan has carets: `^^DEI` and `^LANGUAGE`.
-
-    And so on. We'll probably list these oddities somewhere else, not here.
+    plans is somewhat complicated.
 
     Splitting course names is also necessary to get the prerequisites for the
     course.
     """
-    # Temporary naïve approach
-    if " " in name:
-        subject, number = name.strip("*^").split(maxsplit=1)
-        return subject, number if number[0].isnumeric() else ""
-    else:
-        return "", ""
+    # Based on
+    # https://github.com/SheepTester-forks/ExploratoryCurricularAnalytics/blob/a9e6d0d7afb74f217b3efb382ed39cdd86fe0559/course_names.py#L13-L37
+    name = name.strip("^* ")
+    if name.startswith("ADV. CHEM"):
+        return None
+    name = re.sub(r"DF-?\d - ", "", name)
+    match = re.search(
+        r"\b([A-Z]{2,4}) *(\d+[A-Z]{0,2})(?: *[&/] *\d?[A-Z]([LX]))?", name
+    )
+    if match:
+        subject, number, has_lab = match.group(1, 2, 3)
+        if subject in ["IE", "RR"]:
+            return None
+        return subject, number, has_lab if has_lab == "L" or has_lab == "X" else None
+    return None
