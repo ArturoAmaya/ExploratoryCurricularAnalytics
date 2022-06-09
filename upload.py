f0de7934f081ceaf0e304ad5fe24d12e4f5d41f2
Sean <ussa.say@gmail.com>
2022-06-09 16:46:33 -0700

    Put cookie and auth token in .env
    So I don't almost commit it
    
    Using dotenv because I'm probably the only one running it

--- upload.py
+++ upload.py
@@ -1,5 +1,22 @@
+import os
 from typing import Dict, Tuple, Union
+from urllib.error import HTTPError
 from urllib.request import Request, urlopen
+
+from dotenv import load_dotenv  # type: ignore
+
+load_dotenv()
+
+authenticity_token = os.getenv("AUTHENTICITY_TOKEN")
+if authenticity_token is None:
+    raise EnvironmentError(
+        "There is no `AUTHENTICITY_TOKEN` environment variable defined. See the README to see how to set up `.env`."
+    )
+session = os.getenv("CA_SESSION")
+if session is None:
+    raise EnvironmentError(
+        "There is no `CA_SESSION` environment variable defined. See the README to see how to set up `.env`."
+    )
 
 BOUNDARY = "BOUNDARY"
 LINE = b"\r\n"
@@ -33,24 +50,32 @@ def post_form(
             body += LINE
     body += f"--{BOUNDARY}--".encode("utf-8")
     body += LINE
-    with urlopen(
-        Request(
-            url,
-            headers={
-                "Content-Type": f"multipart/form-data; boundary={BOUNDARY}",
-                **headers,
-            },
-            data=body,
-            method="POST",
-        )
-    ) as response:
-        print(response.read())
+    try:
+        with urlopen(
+            Request(
+                url,
+                headers={
+                    "Content-Type": f"multipart/form-data; boundary={BOUNDARY}",
+                    **headers,
+                },
+                data=body,
+                method="POST",
+            )
+        ) as response:
+            if response.url == "https://curricularanalytics.org/users/sign_in":
+                raise RuntimeError(
+                    "Curricular Analytics isn't recognizing your `CA_SESSION` environment variable. Could you try getting the session cookie again? See the README for how."
+                )
+    except HTTPError as error:
+        raise RuntimeError(
+            "Curricular Analytics doesn't seem to accept your `AUTHENTICITY_TOKEN` environment variable. Could you try getting a new one? See the README for how."
+        ) if error.code == 422 else error
 
 
 post_form(
     "https://curricularanalytics.org/degree_plans",
     {
-        "authenticity_token": "NO.",
+        "authenticity_token": authenticity_token,
         "degree_plan[name]": "apple cider",
         "degree_plan[curriculum_id]": "19353",
         "entry_method": "csv_file",
@@ -58,5 +83,5 @@ post_form(
         "curriculum_json": "",
         "commit": "Save",
     },
-    headers={"cookie": "CENSORED I FOOL"},
+    headers={"cookie": f"_curricularanalytics_session={session}"},
 )
