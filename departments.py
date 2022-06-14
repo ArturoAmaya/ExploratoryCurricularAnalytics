1204a6c111b5c3da45d761a4bdb4f9351532c356
Sean <ussa.say@gmail.com>
2022-06-14 14:50:32 -0700

    Department codes and names from plans.ucsd.edu

---
+++ departments.py
@@ -0,0 +1,16 @@
+"""
+Loads department codes and names from a JSON file saved from plans.ucsd.edu:
+https://plans.ucsd.edu/controller.php?action=LoadSearchControls
+
+Exports:
+    `departments`, a dictionary mapping from department codes to their names.
+"""
+
+import json
+from typing import Dict
+
+
+departments: Dict[str, str] = {}
+with open("./files/LoadSearchControls.json") as controls:
+    for department in json.load(controls)["departments"]:
+        departments[department["code"]] = department["name"]
